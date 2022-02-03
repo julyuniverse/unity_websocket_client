2ac575c24f9836b67145d894fad71d2c3790f4b4
leetsxpro <leetsxpro@gmail.com>
2022-02-03 18:51:00 +0900

    2022-02-03 Update v1

---
+++ Assets/GameManager.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("LoginPanel")]
+    public InputField IDInputField; // ID
+    public InputField PWInputField; // PW
+
+    [Header("SignUpPanel")]
+    public InputField SignUpIDInputField; // 회원가입 ID
+    public InputField SignUpPWInputField; // 회원가입 PW
+
+    public GameObject SignUpPanelObj; // 회원가입 패널
+
+    public string LoginUrl;
+    public string SignUpUrl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoginUrl = "localhost:3000/api/auth/login";
+        SignUpUrl = "loclahost:3000/api/auth/signup";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 로그인 버튼
+    public void LoginBtn()
+    {
+        StartCoroutine(LoginCo());
+    }
+
+    IEnumerator LoginCo()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("id", IDInputField.text);
+        form.AddField("pw", PWInputField.text);
+
+        UnityWebRequest www = UnityWebRequest.Post(LoginUrl, form);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+
+    // 회원가입 버튼
+    public void ToSignUpBtn()
+    {
+        SignUpPanelObj.SetActive(true); // 회원가입 패널 활성화
+    }
+
+    public void SignUpBtn()
+    {
+        StartCoroutine(SignUpCo());
+    }
+
+    IEnumerator SignUpCo()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("id", SignUpIDInputField.text);
+        form.AddField("pw", SignUpPWInputField.text);
+
+        UnityWebRequest www = UnityWebRequest.Post(SignUpUrl, form);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+
+    // 회원가입 취소 버튼
+    public void SignUpCancelBtn()
+    {
+        SignUpPanelObj.SetActive(false); // 회원가입 패널 활성화
+    }
+
+}
---
+++ Assets/Scripts/AuthenticateUser.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AuthenticateUser : MonoBehaviour
+{
+    public InputField displayNameInput, userNameInput, passwordInput;
+
+    // 계정 이름과 비밀번호로 로그인
+    public void AuthorizeUserBtn()
+    {
+
+    }
+}
